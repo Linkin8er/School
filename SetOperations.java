3605236d88a510711c1cab1c953617afd3b8cab2
Linin8er <nick.stephani@gmail.com>
2023-02-22 13:04:32 -0500

    Updated with new assignemnts, as well as Implimented mehtods. Details within

---
+++ SetOperations.java
@@ -0,0 +1,10 @@
+/*
+*   02/20/2023
+*   Set operations class, requirements:
+*   Pick a data type, int or string
+*	Can used days of the week
+*   Write a method that unions two arraylists
+*   Write a method that intersects two arraylists
+*   Write a method that intersects the complement of an array list
+*	Write a method that accepts 2 parameters: an arraylist of all values in your sample. And one that accepts A, your subset. Use that to solve for your compliment. 
+ */
--- StatsLibrary.java
+++ StatsLibrary.java
@@ -9,9 +9,22 @@
 *   Also, after finnishing the list and getting the mean, it immediately restarts you in the mean function 
 */
 
+/*
+*   02/20/2023
+*   It has been way too long since I worked on this
+*   There is still a bug in the choice checker, but it isnt breaking, so progress time
+*   I HAVE FIXED THE BUG, AW YEAHHHHHH
+*   Median function, arraylist sorter, and mode function are implimented.
+*/
+
+/*
+*   02/22/2023
+*   Implimented the Mode method using a hashmap. Very useful! Will nead to learn more about them
+*/
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.HashMap;
 
 //This just runs the stats wizard
 public class StatsLibrary {
@@ -23,31 +36,34 @@ public class StatsLibrary {
 
 class StatsWizard{
 
-    private int userChoice;
+    private double userChoice;
     Scanner scan = new Scanner(System.in);
-    //Constructor time! This runs continuously unti the user opts out. Currently only the mean works
+    //Constructor time! This runs continuously unti the user opts out. 
     StatsWizard(){
         System.out.println("Hello there! I hear you may have some assignments, would you like to do?");
         boolean wantsToContinue = true;
         
         while(wantsToContinue)
         {
-            System.out.println("1) Find a mean\n2) Find a median\n3) Find a mode\n4) End");
-            userChoice = choiceChecker(1,4);
+            System.out.println("1) Find a mean\n2) Find a median\n3) Find a mode\n4) Find a combination\n5) Find a permutation\n6) Find a binomial distribution\n7) End");
+            userChoice = choiceChecker(1,7);
             if(userChoice == 1) findMean();
             if(userChoice == 2) findMedian();
             if(userChoice == 3) findMode();
-            if(userChoice == 4) wantsToContinue = false;
+            if(userChoice == 4) findCombination();
+            if(userChoice == 5) findPermutation();
+            if(userChoice == 6) findBinomialDistribution();
+            if(userChoice == 7) wantsToContinue = false;
         }
         System.out.println("\n\nHave a great day!");
     }
 
     public void findMean(){
         
-        ArrayList<Integer> dataSet = listMaker();
+        ArrayList<Double> dataSet = listMaker();
         
         double dSum = 0;
-        for(int iSingleElement : dataSet){
+        for(double iSingleElement : dataSet){
             dSum = dSum + iSingleElement;
         }
         
@@ -57,54 +73,84 @@ class StatsWizard{
     //Not implimented yet
     public void findMedian(){
 
-        ArrayList<Integer> dataSet = listMaker();
-        
-        int listSize = dataSet.size();
-
-        
+        ArrayList<Double> dataSet = listSorter(listMaker());
         double dMedian = 0;
 
-        
+        //Find the celing of size divided by 2 and that is the median
+        if(dataSet.size()%2 == 1)dMedian = dataSet.get((int)(Math.ceil(dataSet.size()/2.0))-1);
+        //take the average of the size divided by 2 and divided by 2 + 1
+        else if(dataSet.size()%2 == 0 && dataSet.size() != 0)dMedian = (dataSet.get(dataSet.size()/2-1)+dataSet.get((dataSet.size()/2)))/2.0;
+        else System.out.println("There is no median of nothing!");
         System.out.println("The median of your data set is "+ dMedian+"!\n\n");
     }
 
-    //Not implimented yet
-    public void findMode(){
+    public double findMode(){
+
+        ArrayList<Double> dataSet = listMaker();
+
+        HashMap<Double,Integer> modeTracker = new HashMap<Double,Integer>();
+
+        int max  = 1;
+        double temp = 0;
+
+        for(int i = 0; i < dataSet.size(); i++) {
+
+            if (modeTracker.get(dataSet.get(i)) != null) {
+
+                int count = modeTracker.get(dataSet.get(i));
+                count++;
+                modeTracker.put(dataSet.get(i), count);
+
+                if(count > max) {
+                    max  = count;
+                    temp = dataSet.get(i);
+                }
+            }
+
+            modeTracker.put(dataSet.get(i), 1);
+        }
+        return temp;
+    }
+
+    public void findCombination(){
+
+    }
+
+    public void findPermutation(){
         
     }
 
-    public void combinationSolver(){
+    public void birthdayChecker(){
 
     }
 
-    public void permutationSolver(){
-        
-    }
+    //use the permutation and combination solver as needed to help with redundancy
+    public void findBinomialDistribution(){
 
+    }
     //This runs though and makes sure the user inputs legal choices when picking numbers of some kind. 
     
-    public int choiceChecker(int nLowerBounds, int nUpperBounds)
+    public double choiceChecker(int nLowerBounds, int nUpperBounds)
     {
         String userInputString = "";
-        int userInputInt = 0;
+        double userInputDouble = 0;
         boolean userCompliance = false;
         Boolean noBounds = false;
         //This try block is used to not only make sure that the user enters a number, but also that the number follows the set parameters
         while(!userCompliance)
         {
-            userInputInt = 0;
             try
             {
                 //If the upper and lower bounds are both -100, then that indicates that we don't have any bounds, as -100 should never end up being the real bounds
                 if(nLowerBounds == -100 && nUpperBounds == -100) noBounds = true;
-                if(noBounds) System.out.println("Please enter an integer");
+                if(noBounds) System.out.println("Please enter an your number");
                 else System.out.println("Please enter an integer from " + nLowerBounds + " to " + nUpperBounds+ ".");
                 //This set is used to make sure the user enters an integer. If i call for an integer and they input anything else, the program keeps breaking
                 //This way, I can call for a string, and then just change it into an int. If that doesn't work, then I can handle it and work from there.
                 userInputString = scan.nextLine();
-                userInputInt = Integer.parseInt(userInputString);
+                userInputDouble = Double.parseDouble(userInputString); 
                 //This here is the base case for this
-                if((userInputInt >= nLowerBounds && userInputInt <= nUpperBounds) || (noBounds)) userCompliance = true;
+                if((userInputDouble >= nLowerBounds && userInputDouble <= nUpperBounds) || (noBounds)) userCompliance = true;
                 //This else will run if the user enters a number that is outside the bounds in which case it uses recursion until the user complies
                 else System.out.println("That was not within the bounds");
             }
@@ -115,7 +161,7 @@ class StatsWizard{
                 System.out.println("Please enter a proper number next time.");
             }
         }        
-        return userInputInt;
+        return userInputDouble;
     }
     
 
@@ -123,7 +169,7 @@ class StatsWizard{
     //It uses the choice checker to make sure that the user inputs proper answers. 
     public ArrayList listMaker(){
 
-        ArrayList<Integer> dataSet = new ArrayList();
+        ArrayList<Double> dataSet = new ArrayList();
         System.out.println("\nWhat is in your dataset?\n");
         boolean bHasMoreInputs = true;
 
@@ -133,14 +179,25 @@ class StatsWizard{
             for (int iIterator =0; iIterator < dataSet.size(); iIterator++) System.out.print(dataSet.get(iIterator) + " ,");
             System.out.println(" so far. Would you like to enter more?\n1) Yes!\n2) No, I'm Done.");
             userChoice = choiceChecker(1,2);
-            if(userChoice == 1){
-                System.out.println("Please enter the number you would like to add");
-                dataSet.add(scan.nextInt());
-            }
+            if(userChoice == 1) dataSet.add(choiceChecker(-100,-100));
             if(userChoice == 2) bHasMoreInputs = false;
         }
         userChoice = 0;
         return dataSet;
+    }
+    public ArrayList listSorter(ArrayList<Double> unsorted){
+
+        for( int i = 0; i < unsorted.size();  i++){
+            double key = unsorted.get(i);
+		    //insert A[i] into the sorted subarray A[0:i-1]
+            int j = i-1;
+            while( j>=0 && unsorted.get(j) > key){
+                unsorted.set(j+1, unsorted.get(j));
+                j = j-1;
+            }
+            unsorted.set(j+1, key);
+        }
+        return unsorted;
     }
 }
 
