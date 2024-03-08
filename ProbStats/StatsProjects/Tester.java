0e0ae817eb23387bf38fc94670f8691bfdc3f656
Linkin8er <nick.stephani@gmail.com>
2024-03-07 23:19:38 -0500

    further documentation changes

--- ProbStats/StatsProjects/Tester.java
+++ ProbStats/StatsProjects/Tester.java
@@ -1,17 +1,19 @@
+import java.util.Scanner;
 public class Tester {
+
+    Scanner scan = new Scanner(System.in);
     public static void main(String[] args){
 
-        //StatsLibrary statsTester = new StatsLibrary();
+        StatsLibrary statsTester = new StatsLibrary();
         PokemonCardGame testGame = new PokemonCardGame();
-        //MonteCarloPokeTester test = new MonteCarloPokeTester(15);
-        //MonteCarloBrickDeck bricker = new MonteCarloBrickDeck(5);
+        
+        MonteCarloPokeTester test = new MonteCarloPokeTester(15);
+        MonteCarloBrickDeck bricker = new MonteCarloBrickDeck(5);
         
         testGame.runGame();
 
-        //testGame.preGame();
-        //testGame.startOfGame();
-        //testGame.gameRounds();
+        statsTester.operationPicker();
 
-        //statsTester.operationPicker();
     }
+    
 }
