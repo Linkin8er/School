b752ed8e904a06363fe1c9bec023b79d2e4be74e
Linkin8er <nick.stephani@gmail.com>
2024-04-18 16:39:19 -0400

    Starting work on the facial generation

--- FaceGenerationFinal.py
+++ FaceGenerationFinal.py
@@ -1,255 +1,125 @@
+import pandas
 import numpy as np
-import torch
 import matplotlib.pyplot as plt
-from torchvision import datasets
-import torchvision.transforms as transforms
-
-# number of subprocesses to use for data loading
-num_workers = 0
-# how many samples per batch to load
-batch_size = 64
-# convert data to torch.FloatTensor
-transform = transforms.ToTensor()
-# get the training datasets
-train_data = datasets.MNIST(root='data', train=True,
-download=True, transform=transform)
-# prepare data loader
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
-num_workers=num_workers)
-
-# obtain one batch of training images
-dataiter = iter(train_loader)
-images, labels = next(dataiter)
-images = images.numpy()
-# get one image from the batch
-img = np.squeeze(images[0])
-fig = plt.figure(figsize = (3,3))
-ax = fig.add_subplot(111)
-ax.imshow(img, cmap='gray')
-
-import torch.nn as nn
-import torch.nn.functional as F
-
-class Discriminator(nn.Module):
-  def __init__(self, input_size, hidden_dim, output_size):
-    super(Discriminator, self).__init__()
-    # define hidden linear layers
-    self.fc1 = nn.Linear(input_size, hidden_dim*4)
-    self.fc2 = nn.Linear(hidden_dim*4, hidden_dim*2)
-    self.fc3 = nn.Linear(hidden_dim*2, hidden_dim)
-    # final fully-connected layer
-    self.fc4 = nn.Linear(hidden_dim, output_size)
-    # dropout layer
-    self.dropout = nn.Dropout(0.3)
-  def forward(self, x):
-    # flatten image
-    x = x.view(-1, 28*28)
-    # all hidden layers
-    x = F.leaky_relu(self.fc1(x), 0.2) # (input, negative_slope=0.2)
-    x = self.dropout(x)
-    x = F.leaky_relu(self.fc2(x), 0.2)
-    x = self.dropout(x)
-    x = F.leaky_relu(self.fc3(x), 0.2)
-    x = self.dropout(x)
-    # final layer
-    out = self.fc4(x)
-    return out
-  
-class Generator(nn.Module):
-  def __init__(self, input_size, hidden_dim, output_size):
-    super(Generator, self).__init__()
-    # define hidden linear layers
-    self.fc1 = nn.Linear(input_size, hidden_dim)
-    self.fc2 = nn.Linear(hidden_dim, hidden_dim*2)
-    self.fc3 = nn.Linear(hidden_dim*2, hidden_dim*4)
-    # final fully-connected layer
-    self.fc4 = nn.Linear(hidden_dim*4, output_size)
-    # dropout layer
-    self.dropout = nn.Dropout(0.3)
-  def forward(self, x):
-    # all hidden layers
-    x = F.leaky_relu(self.fc1(x), 0.2) # (input, negative_slope=0.2)
-    x = self.dropout(x)
-    x = F.leaky_relu(self.fc2(x), 0.2)
-    x = self.dropout(x)
-    x = F.leaky_relu(self.fc3(x), 0.2)
-    x = self.dropout(x)
-    # final layer with tanh applied
-    out = F.tanh(self.fc4(x))
-    return out
-
-# Discriminator hyperparams
-# Size of input image to discriminator (28*28)
-input_size = 784
-# Size of discriminator output (real or fake)
-d_output_size = 1
-# Size of last hidden layer in the discriminator
-d_hidden_size = 32
-# Generator hyperparams
-# Size of latent vector to give to generator
-z_size = 100
-# Size of discriminator output (generated image)
-g_output_size = 784
-# Size of first hidden layer in the generator
-g_hidden_size = 32
-
-# instantiate discriminator and generator
-D = Discriminator(input_size, d_hidden_size, d_output_size)
-G = Generator(z_size, g_hidden_size, g_output_size)
-# check that they are as you expect
-print(D)
-print()
-print(G)
-
 import tensorflow as tf
-# Calculate losses
-def real_loss(D_out, smooth=False):
-  batch_size = D_out.size(0)
-  # label smoothing
-  if smooth:
-    # smooth, real labels = 0.9
-    labels = torch.ones(batch_size)*0.9
-  else:
-    labels = torch.ones(batch_size) # real labels = 1
-  # numerically stable loss
-  criterion = nn.BCEWithLogitsLoss()
-  # calculate loss
-  loss = criterion(D_out.squeeze(), labels)
-  return loss
+from tqdm import tqdm
+import os
+import cv2
+from tensorflow import keras 
+import tensorflow as tf
+from keras import layers, Sequential, datasets, Model
 
-def fake_loss(D_out):
-  batch_size = D_out.size(0)
-  labels = torch.zeros(batch_size) # fake labels = 0
-  criterion = nn.BCEWithLogitsLoss()
-  # calculate loss
-  loss = criterion(D_out.squeeze(), labels)
-  return loss
+dir_path = r"C:/Computer Vision/img_align_celeba/img_align_celeba"
+def load_images(folder_path, img_size = (128, 128)):
+    X = []
+    limit = 50000
+    for img_name in tqdm(os.listdir(folder_path)):
+        img_path = os.path.join(folder_path, img_name)
+        img_array = cv2.imread(img_path)
+        img_array = cv2.resize(img_array, img_size)
+        img_array = img_array[:,:,::-1]
+        X.append(img_array / 255.0)
+        if len(X) >= limit:
+            break
+    return np.array(X)
 
-import torch.optim as optim
-# Optimizers
-lr = 0.002
-# Create optimizers for the discriminator and generator
-d_optimizer = optim.Adam(D.parameters(), lr)
-g_optimizer = optim.Adam(G.parameters(), lr)
+img_size = 64
+X_train = load_images(dir_path, (img_size, img_size))
+X_train.shape
 
-import pickle as pkl
-# training hyperparams
-num_epochs = 100
-# keep track of loss and generated, "fake" samples
-samples = []
-losses = []
-print_every = 400
-# Get some fixed data for sampling. These are images that are held
-# constant throughout training, and allow us to inspect the model's performance
-sample_size=16
-fixed_z = np.random.uniform(-1, 1, size=(sample_size, z_size))
-fixed_z = torch.from_numpy(fixed_z).float()
-# train the network
-D.train()
-G.train()
-for epoch in range(num_epochs):
-  for batch_i, (real_images, _) in enumerate(train_loader):
-    batch_size = real_images.size(0)
-    ## Important rescaling step ##
-    real_images = real_images*2 - 1 # rescale input images from [0,1) to [-1, 1)
-    # ============================================
-    # TRAIN THE DISCRIMINATOR
-    # ============================================
+fixed_noise = tf.random.normal((60, 128))
 
-    d_optimizer.zero_grad()
-    # 1. Train with real images
+def show_images(images):
+    for i in range(20):
+        plt.subplot(4, 5, i + 1)
+        plt.imshow(images[i])
+        plt.xticks([])
+        plt.yticks([])
+    plt.show()
+show_images(X_train)
 
-    # Compute the discriminator losses on real images
-    # smooth the real labels
-    D_real = D(real_images)
-    d_real_loss = real_loss(D_real, smooth=True)
-    # 2. Train with fake images
+Discriminator = Sequential([
+    layers.Conv2D(256, kernel_size = (3, 3), strides = 2, padding = 'same', input_shape = (64, 64, 3)),
+    layers.LeakyReLU(),
 
-    # Generate fake images
-    # gradients don't have to flow during this step
-    with torch.no_grad():
-      z = np.random.uniform(-1, 1, size=(batch_size, z_size))
-      z = torch.from_numpy(z).float()
-      fake_images = G(z)
-    # Compute the discriminator losses on fake images
-    D_fake = D(fake_images)
-    d_fake_loss = fake_loss(D_fake)
-    # add up loss and perform backprop
-    d_loss = d_real_loss + d_fake_loss
-    d_loss.backward()
-    d_optimizer.step()
-    # =========================================
-    # TRAIN THE GENERATOR
-    # =========================================
-    g_optimizer.zero_grad()
-    # 1. Train with fake images and flipped labels
-    # Generate fake images
-    z = np.random.uniform(-1, 1, size=(batch_size, z_size))
-    z = torch.from_numpy(z).float()
-    fake_images = G(z)
-    # Compute the discriminator losses on fake images
-    # using flipped labels!
-    D_fake = D(fake_images)
-    g_loss = real_loss(D_fake) # use real loss to flip labels
-    # perform backprop
-    g_loss.backward()
-    g_optimizer.step()
-    # Print some loss stats
-    if batch_i % print_every == 0:
-      # print discriminator and generator loss
-      print('Epoch [{:5d}/{:5d}] | d_loss: {:6.4f} | g_loss: {:6.4f}'.format(
-      epoch+1, num_epochs, d_loss.item(), g_loss.item()))
-  ## AFTER EACH EPOCH##
-  # append discriminator loss and generator loss
-  losses.append((d_loss.item(), g_loss.item()))
-  # generate and save sample, fake images
-  G.eval() # eval mode for generating samples
-  samples_z = G(fixed_z)
-  samples.append(samples_z)
-  G.train() # back to train mode
-# Save training generator samples
-with open('train_samples.pkl', 'wb') as f:
-  pkl.dump(samples, f)
+    layers.Conv2D(128, kernel_size = (3, 3), strides = 2, padding = 'same'),
+    layers.LeakyReLU(),
+    layers.BatchNormalization(),
 
-fig, ax = plt.subplots()
-losses = np.array(losses)
-plt.plot(losses.T[0], label='Discriminator')
-plt.plot(losses.T[1], label='Generator')
-plt.title("Training Losses")
-plt.legend()
+    layers.Conv2D(64, kernel_size = (3, 3), strides = 2, padding = 'same'),
+    layers.LeakyReLU(),
+    layers.BatchNormalization(),
 
-# helper function for viewing a list of passed in sample images
-def view_samples(epoch, samples):
-  fig, axes = plt.subplots(figsize=(7,7), nrows=4, ncols=4, sharey=True, sharex=True)
-  for ax, img in zip(axes.flatten(), samples[epoch]):
-    img = img.detach()
-    ax.xaxis.set_visible(False)
-    ax.yaxis.set_visible(False)
-    im = ax.imshow(img.reshape((28,28)), cmap='Greys_r')
+    layers.Flatten(),
+    layers.Dense(1)
+])
 
-with open('train_samples.pkl', 'rb') as f:
-  samples = pkl.load(f)
+Generator = Sequential([
+    layers.Dense(8 * 8 * 128, input_shape = (128,)),
+    layers.BatchNormalization(),
 
-# -1 indicates final epoch's samples (the last in the list)
-view_samples(-1, samples)
+    layers.Reshape((8, 8, 128)),
+    layers.Conv2DTranspose(256, kernel_size = (3, 3), strides = 2, padding = 'same'),
+    layers.LeakyReLU(),
+    layers.BatchNormalization(),
 
-rows = 10 # split epochs into 10, so 100/10 = every 10 epochs
-cols = 6
-fig, axes = plt.subplots(figsize=(7,12), nrows=rows, ncols=cols, sharex=True, sharey=True)
-for sample, ax_row in zip(samples[::int(len(samples)/rows)], axes):
-  for img, ax in zip(sample[::int(len(sample)/cols)], ax_row):
-    img = img.detach()
-    ax.imshow(img.reshape((28,28)), cmap='Greys_r')
-    ax.xaxis.set_visible(False)
-    ax.yaxis.set_visible(False)
+    layers.Conv2DTranspose(128, kernel_size = (3, 3), strides = 2, padding = 'same'),
+    layers.LeakyReLU(),
+    layers.BatchNormalization(),
 
-# randomly generated, new latent vectors
-sample_size=16
-rand_z = np.random.uniform(-1, 1, size=(sample_size, z_size))
-rand_z = torch.from_numpy(rand_z).float()
-G.eval() # eval mode
-# generated samples
-rand_images = G(rand_z)
-# 0 indicates the first set of samples in the passed in list
-# and we only have one batch of samples, here
-view_samples(0, [rand_images])
+
+    layers.Conv2DTranspose(3, kernel_size = (3, 3), strides = 2, padding = 'same', activation = 'sigmoid'),
+])
+
+class GAN(Model):
+    def __init__(self, Generator, Discriminator):
+        super().__init__()
+        self.latent_dim = 128
+        self.generator = Generator
+        self.discriminator = Discriminator
+        # print(self.generator.model.summary())
+    def compile(self, gen_optimizer, disc_optimizer, criterion):
+        super().compile()
+        self.generator_optimizer = gen_optimizer
+        self.discriminator_optimizer = disc_optimizer
+        self.cross_entropy = criterion
+
+    def discriminator_loss(self, real_output, fake_output):
+        real_loss = self.cross_entropy(tf.ones_like(real_output), real_output)
+        fake_loss = self.cross_entropy(tf.zeros_like(fake_output), fake_output)
+        total_loss = real_loss + fake_loss
+        return total_loss
+
+
+    def generator_loss(self, fake_output):
+        return self.cross_entropy(tf.ones_like(fake_output), fake_output)
+
+    def train_step(self, images):
+        
+        noise = tf.random.normal([images.shape[0], self.latent_dim])
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            generated_images = self.generator(noise, training=True)
+
+            real_output = self.discriminator(images, training=True)
+            fake_output = self.discriminator(generated_images, training=True)
+
+            gen_loss = self.generator_loss(fake_output)
+            disc_loss = self.discriminator_loss(real_output, fake_output)
+
+        gradients_of_generator = gen_tape.gradient(gen_loss, self.generator.trainable_variables)
+        gradients_of_discriminator = disc_tape.gradient(disc_loss, self.discriminator.trainable_variables)
+
+        self.generator_optimizer.apply_gradients(zip(gradients_of_generator, self.generator.trainable_variables))
+        self.discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, self.discriminator.trainable_variables))
+
+        return {"Generator Loss": gen_loss, "Discriminator Loss": disc_loss}
+    
+model = GAN(Generator, Discriminator)
+model.compile(gen_optimizer = tf.keras.optimizers.Adam(learning_rate = 0.0001),
+              disc_optimizer = tf.keras.optimizers.Adam(learning_rate = 0.0001),
+              criterion = tf.keras.losses.BinaryCrossentropy(True))
+
+model.fit(X_train, epochs = 100, batch_size = 100)
+
+y_pred = model.generator.predict(fixed_noise)
+show_images(y_pred)
